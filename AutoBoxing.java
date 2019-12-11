d526fd2850e9774fe085b29283d497bfb2d8eb81
BeemaSirajudeen <58411264+BeemaSirajudeen@users.noreply.github.com>
2019-12-11 17:59:05 +0530

    Add files via upload

---
+++ AutoBoxing.java
@@ -0,0 +1,11 @@
+
+public class AutoBoxing {
+
+	public static void main(String[] args) {
+		Integer i=20;           // converting int into Integer
+		Integer j=i;           //Autoboxing ,now compiler will write Integer.valueOf(a) internally
+			System.out.println(i+" "+j);
+
+	}
+
+}
