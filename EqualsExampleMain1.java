7433647d7cbc9ef02d0c65b5bbefe419c877539a
BeemaSirajudeen <58411264+BeemaSirajudeen@users.noreply.github.com>
2019-12-19 15:31:41 +0530

    Add files via upload

---
+++ EqualsExampleMain1.java
@@ -0,0 +1,18 @@
+
+public class EqualsExampleMain1
+{
+
+	public static void main(String[] args)
+	{
+		String s1 = "facenow";
+		String s2 = "facenow";
+		String s3 = "faceprep";
+		String s4 ="facenxt";
+		System.out.println(s1.contentEquals(s2)); //true because content and case are same
+		System.out.println(s1.contentEquals(s3)); //false because  case is not same
+		System.out.println(s1.contentEquals(s4)); //false because content is not same
+		
+		
+	}
+
+}
