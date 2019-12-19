0b0b00be2b05d9e9ee1dccabafc61cbab1b0a51c
BeemaSirajudeen <58411264+BeemaSirajudeen@users.noreply.github.com>
2019-12-19 15:30:15 +0530

    Add files via upload

---
+++ ContainsExampleMain2.java
@@ -0,0 +1,13 @@
+
+public class ContainsExampleMain2 {
+
+	public static void main(String[] args) {
+		String str = "Hello F#A#C#E@2.0 readers";
+		boolean isContains = str.contains("F#A#C#E");
+		System.out.println(isContains);
+		//case sensitive
+		System.out.println(str.contains("FACe"));
+
+	}
+
+}
